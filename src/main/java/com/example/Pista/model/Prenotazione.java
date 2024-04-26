ae8334a69e30f4707a2f75bba37a0463280caba3
Bruno Barone <bruno.barone1996@hotmail.com>
2024-04-26 09:47:31 +0200

    Creazione model Prenotazione con getter e setter

---
+++ src/main/java/com/example/Pista/model/Prenotazione.java
@@ -0,0 +1,99 @@
+package com.example.Pista.model;
+
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "prenotazioni")
+public class Prenotazione {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column
+    private LocalDateTime dataOraCorsa;
+
+
+    @Column
+    private LocalDateTime dataOraPagamento;
+
+
+    @ManyToOne(cascade = CascadeType.REFRESH)
+    @JoinColumn(name = "id_utente", referencedColumnName = "id")
+    private Utente utente;
+
+
+    @OneToOne(cascade = CascadeType.REFRESH)
+    @JoinColumn(name = "id_auto", referencedColumnName = "id")
+    private Auto auto;
+
+
+    @OneToOne(cascade = CascadeType.REFRESH)
+    @JoinColumn(name = "id_pagamento", referencedColumnName = "id")
+    private Pagamento pagamento;
+
+
+    @OneToOne(cascade = CascadeType.REFRESH)
+    @JoinColumn(name = "id_pilota", referencedColumnName = "id")
+    private Pilota pilota;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getDataOraCorsa() {
+        return dataOraCorsa;
+    }
+
+    public void setDataOraCorsa(LocalDateTime dataOraCorsa) {
+        this.dataOraCorsa = dataOraCorsa;
+    }
+
+    public LocalDateTime getDataOraPagamento() {
+        return dataOraPagamento;
+    }
+
+    public void setDataOraPagamento(LocalDateTime dataOraPagamento) {
+        this.dataOraPagamento = dataOraPagamento;
+    }
+
+    public Utente getUtente() {
+        return utente;
+    }
+
+    public void setUtente(Utente utente) {
+        this.utente = utente;
+    }
+
+    public Auto getAuto() {
+        return auto;
+    }
+
+    public void setAuto(Auto auto) {
+        this.auto = auto;
+    }
+
+    public Pagamento getPagamento() {
+        return pagamento;
+    }
+
+    public void setPagamento(Pagamento pagamento) {
+        this.pagamento = pagamento;
+    }
+
+    public Pilota getPilota() {
+        return pilota;
+    }
+
+    public void setPilota(Pilota pilota) {
+        this.pilota = pilota;
+    }
+}
