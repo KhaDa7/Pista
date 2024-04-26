12ac282309f4d499deeb99f2e6c6dad9af62ec5d
KhaDa7 <khalid.darraj99@gmail.com>
2024-04-26 11:25:35 +0200

    Spostamento file html dalla cartella template a templates e creazione homeController

---
+++ src/main/java/com/example/Pista/controller/homeController.java
@@ -0,0 +1,14 @@
+package com.example.Pista.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class homeController {
+    @GetMapping
+    public String getPage() {
+        return "home";
+    }
+}
