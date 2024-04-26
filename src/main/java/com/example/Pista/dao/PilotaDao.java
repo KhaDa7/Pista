752e4e36d9e960b804f596bfee2055c2ac0552eb
KhaDa7 <khalid.darraj99@gmail.com>
2024-04-26 10:06:08 +0200

    Creazione Dao di Profilo e Pilota

---
+++ src/main/java/com/example/Pista/dao/PilotaDao.java
@@ -0,0 +1,7 @@
+package com.example.Pista.dao;
+
+import com.example.Pista.model.Pilota;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PilotaDao extends CrudRepository<Pilota, Long> {
+}
