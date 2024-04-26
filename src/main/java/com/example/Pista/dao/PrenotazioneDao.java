f34df177f03580590ac477477a910f00449514e4
Bruno Barone <bruno.barone1996@hotmail.com>
2024-04-26 10:07:04 +0200

    Creazione PrenotazioneDao e PagamentoDao

---
+++ src/main/java/com/example/Pista/dao/PrenotazioneDao.java
@@ -0,0 +1,7 @@
+package com.example.Pista.dao;
+
+import com.example.Pista.model.Prenotazione;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PrenotazioneDao extends CrudRepository<Prenotazione, Integer> {
+}
