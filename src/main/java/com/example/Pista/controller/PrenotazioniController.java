b7080d6e522e450827a907dbcd8dd4fca2e2ad1c
KhaDa7 <khalid.darraj99@gmail.com>
2024-04-30 21:42:57 +0200

    Modifiche al comportamento della prenotazione
    Iniziale bozza di gestione dei controlli sull'eventuale prenotazione in una data già prenotata

--- src/main/java/com/example/Pista/controller/PrenotazioniController.java
+++ src/main/java/com/example/Pista/controller/PrenotazioniController.java
@@ -1,5 +1,6 @@
 package com.example.Pista.controller;
 
+import com.example.Pista.model.Pilota;
 import com.example.Pista.model.Prenotazione;
 import com.example.Pista.model.Utente;
 import com.example.Pista.service.PrenotazioneService;
@@ -11,6 +12,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
 
 @Controller
 @RequestMapping ("/prenotazioni")
@@ -25,9 +28,12 @@ public class PrenotazioniController
     @GetMapping
     public String getPage (Model model, HttpSession session) {
         Utente utente = (Utente) session.getAttribute("utente");
+        Pilota pilota = (Pilota) session.getAttribute("pilota");
         if(utente == null){
             return "redirect:/login";
         }
+        session.setAttribute("utente", utente);
+        model.addAttribute("pilota", pilota);
         model.addAttribute("utente", utente);
         Prenotazione prenotazione = new Prenotazione();
         model.addAttribute("prenotazione", prenotazione);
@@ -35,8 +41,24 @@ public class PrenotazioniController
     }
 
     @PostMapping
-    public String inviaPage (@RequestParam("auto") int idAuto,@RequestParam("dataCorsa") String data,@RequestParam("pagamento") int idPagamento, HttpSession session) {
-        prenotazioneService.inviaPrenotazione(session,idAuto,data,idPagamento);
-        return "redirect:/riservatautente?send";
+    public String inviaPage (@ModelAttribute("prenotazione") Prenotazione prenotazione,
+                             @RequestParam("auto") int idAuto,
+                             @RequestParam("dataCorsa") String data,
+                             @RequestParam("pilota") int idPilota,
+                             @RequestParam("pagamento") int idPagamento,
+                             HttpSession session,BindingResult result,
+                             Model model) {
+        prenotazioneService.inviaPrenotazione(session,idAuto,data,idPilota,idPagamento);
+        if(result.hasErrors())
+            return "prenotazioni";
+        if(!prenotazioneService.controlloPrenotazione(prenotazione.getDataCorsa()))
+        {
+            model.addAttribute("error", "Data già prenotata");
+            return "prenotazioni";
+        }
+
+        prenotazioneService.registraPrenotazione(prenotazione);
+        return "redirect:/riservatautente";
     }
+
 }
--- src/main/java/com/example/Pista/service/PrenotazioneServiceImpl.java
+++ src/main/java/com/example/Pista/service/PrenotazioneServiceImpl.java
@@ -17,15 +17,19 @@ import java.time.LocalDateTime;
 public class PrenotazioneServiceImpl implements PrenotazioneService {
     @Autowired
     PrenotazioneDao prenotazioneDao;
+
     @Autowired
     AutoDao autoDao;
+
     @Autowired
     PagamentoDao pagamentoDao;
+
     @Autowired
     PilotaDao pilotaDao;
 
+    @SuppressWarnings("OptionalGetWithoutIsPresent")
     @Override
-    public void inviaPrenotazione(HttpSession session, int idAuto, String data, int idPagamento) {
+    public void inviaPrenotazione(HttpSession session, int idAuto, String data, int idPilota, int idPagamento) {
         Utente utente = (Utente) session.getAttribute("utente");
         if(utente != null){
             Prenotazione prenotazione = new Prenotazione();
@@ -33,9 +37,21 @@ public class PrenotazioneServiceImpl implements PrenotazioneService {
             prenotazione.setDataCorsa(LocalDate.parse(data));
             prenotazione.setDataOraPagamento(LocalDateTime.now());
             prenotazione.setAuto(autoDao.findById(idAuto).get());
-            prenotazione.setPilota(pilotaDao.findById(1L).get());
+            prenotazione.setPilota(pilotaDao.findById(1).get());
             prenotazione.setPagamento(pagamentoDao.findById(idPagamento).get());
             prenotazioneDao.save(prenotazione);
         }
     }
+
+    @Override
+    public boolean controlloPrenotazione(LocalDate data) {
+        return false;
+    }
+
+    @Override
+    public void registraPrenotazione(Prenotazione prenotazione) {
+        prenotazioneDao.save(prenotazione);
+    }
+
+
 }
